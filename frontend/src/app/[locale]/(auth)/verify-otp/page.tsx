2b2eabd2b8f44fb11833635a6f7d97c9ce434988
phucdtWork <phucdt.work@gmail.com>
2026-01-20 16:10:38 +0700

    fix: fix ref callback to not return value

--- frontend/src/app/[locale]/(auth)/verify-otp/page.tsx
+++ frontend/src/app/[locale]/(auth)/verify-otp/page.tsx
@@ -199,7 +199,9 @@ export default function VerifyOtpPage() {
               {otp.map((digit, index) => (
                 <input
                   key={index}
-                  ref={(el) => (inputRefs.current[index] = el)}
+                  ref={(el) => {
+                    if (el) inputRefs.current[index] = el;
+                  }}
                   type="text"
                   inputMode="numeric"
                   maxLength={1}
